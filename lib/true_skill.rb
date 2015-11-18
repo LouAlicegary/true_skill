fce801929ce0bd4d7924a63cabc5c16c2110302c
Lou Alicegary <lou@ginkgotree.com>
2015-11-18 16:46:50 -0500

    gem fully working

--- lib/true_skill.rb
+++ lib/true_skill.rb
@@ -1,62 +1,19 @@
-require "true_skill/version"
-require "true_skill/true_skill_general"
+module TrueSkillGeneral
 
-module TrueSkill::TrueSkillGeneral
+    require "true_skill/version"
 
-    #module TrueSkillGeneral
-
-
-      MU=25.0
-      SIGMA=MU/3
-      BETA=SIGMA/2
-      TAU=SIGMA/100
-      DRAW_PROBABILITY=0.10
-      DELTA=0.001
-
-
-      def calc_draw_probability(draw_margin,beta,size)
-        return 2*cdf(draw_margin*(Math.sqrt(size)*beta))-1
-      end
-      
-
-      def calc_draw_margin(draw_probability, beta, size)
-        return ppf((draw_probability+1)/2.0)*Math.sqrt(size)*beta
-      end
-      
-
-      def _team_sizes(rating_groups)
-        team_sizes=[0]
-        rating_groups.each do |group|
-          team_sizes << group.length+team_sizes.last
-        end
-        team_sizes.delete_at(0)
-        return team_sizes
-      end
-
-
-      $global=[]
-
-
-      def g()
-        if $global.length==0
-          setup()
-        end
-        return $global[0]
-      end
-      
-
-      def setup(mu=MU, sigma=SIGMA, beta=BETA, tau=TAU,draw_probability=DRAW_PROBABILITY, env=nil)
-        $global.pop
-        if env.nil?
-          $global << TrueSkillObject.new(mu, sigma, beta, tau, draw_probability)
-        else
-          $global << env
-        end
-        return g()
-      end
-
-
-
-    #end
+    require "true_skill/array"
+    require "true_skill/factor"
+    require "true_skill/gaussian"
+    require "true_skill/likelihood_factor"
+    require "true_skill/math_general"
+    require "true_skill/matrix"
+    require "true_skill/prior_factor"
+    require "true_skill/rating"
+    require "true_skill/sum_factor"
+    require "true_skill/true_skill_general"
+    require "true_skill/true_skill_object"
+    require "true_skill/truncate_factor"
+    require "true_skill/variable"
 
 end
